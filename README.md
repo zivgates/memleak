5a6e46586784925d438076eb3763aaf6ad31b286
realzvqle <realzvqle@gmail.com>
2024-06-17 20:38:46 -0500

    preference

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ to leak 10KB/ms of non-paged memory (forced to the RAM till it fails), run this
 ```
 memleak 10 nonpaged
 ```
+
+
+(be aware memleak with nonpaged will start to fail when windows decides that the process cannot keep the memory into RAM, and memleak will fail alltogethor when the windows hits it with OOM)
--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <Windows.h>
 #include <wchar.h>
 
-
 INT wmain(INT argc, WCHAR* argv[]){
     if(argc < 3){
         wprintf(L"usage: memleak [kilobytes] [paged or not]\n");
@@ -17,10 +16,14 @@ INT wmain(INT argc, WCHAR* argv[]){
     }
     size *= 1024;
     ULONG totalAllocated = 0;
-
+    BOOL result = SetProcessWorkingSetSize(GetCurrentProcess(), 1024 * 1024, 1024 * 1024 * 1000);
+    if(!result){
+        wprintf(L"Couldn't Set The Process Working Size\n");
+        return -1;
+    }
     while(1){
         totalAllocated+=size;
-        LPVOID ptr = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
+        LPVOID ptr = HeapAlloc(GetProcessHeap(), 0, size);
         if(!ptr){
             wprintf(L"[-] Error Allocating Memory\n");
             return -1;
@@ -28,7 +31,7 @@ INT wmain(INT argc, WCHAR* argv[]){
         if(isPaged == TRUE){
             BOOL result = VirtualLock(ptr, size);
             if(!result){
-                wprintf(L"[-] Failed Locking Memory into RAM\n");
+                wprintf(L"[-] Failed Locking Memory into RAM, Error Code %d\n", GetLastError());
             }
         }
         wprintf(L"[+] Succsessfully Created %p with size of %d Kilobytes, Leaked %d Kilobytes\n", ptr, size / 1024, totalAllocated / 1024);
